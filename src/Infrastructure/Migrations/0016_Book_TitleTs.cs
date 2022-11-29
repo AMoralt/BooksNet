f9e9ffb23b7ca4b2c91fda28feae68e54b2c5328
AMoralt <dias_galym@bk.ru>
2022-11-29 19:08:54 +0500

    Add tsvector for books table on title column and also index on it

---
+++ src/Infrastructure/Migrations/0016_Book_TitleTs.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace Infrastructure.Migrations;
+
+[Migration(16)]
+public class Book_TitleTs: Migration
+{
+    public override void Up()
+    {
+        Execute.Sql(@"ALTER TABLE books
+            ADD COLUMN if not exists title_ts tsvector
+            GENERATED ALWAYS AS (to_tsvector('russian', title)) STORED");
+    }
+
+    public override void Down()
+    {
+        Execute.Sql(@"DROP TABLE if exists books");
+    }
+}
